f7e077e9ae94ef3115fb0b13bf6209085fd49322
Ben Wainaina <bwainaina@curalinc.com>
2026-01-12 14:37:33 +0300

    update prod file

--- prod.sh
+++ prod.sh
@@ -2,4 +2,4 @@ source .app_env/bin/activate
 serviceName="tiktokfans"
 pm2 stop $serviceName
 pm2 delete $serviceName
-pm2 start uvicorn --name $serviceName --interpreter python3 -- server:SERVER --host 0.0.0.0 --port 8001
+pm2 start uvicorn --name $serviceName --interpreter python3 -- server:SERVER --host 0.0.0.0 --port 8003
--- tiktok_listener.py
+++ tiktok_listener.py
@@ -1,6 +1,9 @@
 from TikTokLive import TikTokLiveClient
-from TikTokLive.events import ConnectEvent, FollowEvent, GiftEvent, LikeEvent
+from TikTokLive.client.web.web_settings import WebDefaults
+from TikTokLive.events import ConnectEvent, FollowEvent, GiftEvent, LikeEvent, CommentEvent
 from fastapi import WebSocket
+
+WebDefaults.tiktok_sign_api_key = 'euler_YzA0MzlkNGEwYTlhODgxMzVjZDk4MmNjY2QzMDE5YWE1OWQ5MmQzZGVkMDNkOWNlODk5Y2M3'
 
 class TikTokListener:
     def __init__(self, websocket: WebSocket, username: str):
@@ -12,10 +15,11 @@ class TikTokListener:
         await self.client.start()
 
         # trigger event listeners
-        self.client.add_listener(ConnectEvent, self.on_connect)
+        # self.client.add_listener(ConnectEvent, self.on_connect)
         self.client.add_listener(LikeEvent, self.on_like_event)
-        self.client.add_listener(GiftEvent, self.on_gift_event)
+        # self.client.add_listener(GiftEvent, self.on_gift_event)
         self.client.add_listener(FollowEvent, self.on_follow_event)
+        # self.client.add_listener(CommentEvent, self.on_comment_event)
 
     async def close_client(self):
         await self.client.disconnect()
@@ -41,7 +45,11 @@ class TikTokListener:
             'name': event.user.nick_name,
             'username': event.user.username,
             'avatar': event.user.avatar_thumb.m_urls[0],
-            'event_type': 'gift'
+            'event_type': 'gift',
+            'gift': {
+                'repeat': event.repeat_count,
+                'name': event.gift.name,
+            }
         })
 
     async def on_follow_event(self, event: FollowEvent):
@@ -50,6 +58,15 @@ class TikTokListener:
             'username': event.user.username,
             'avatar': event.user.avatar_thumb.m_urls[0],
             'event_type': 'follow'
+        })
+
+    async def on_comment_event(self, event: CommentEvent):
+        await self.send_event({
+            'name': event.user_info.nick_name,
+            'username': event.user_info.username,
+            'avatar': event.user_info.avatar_thumb.m_urls[0],
+            'event_type': 'comment',
+            'comment': event.comment
         })
 
     async def send_event(self, event):
