926866c508653c83d4184377ad881494023e6190
Ben Wainaina <bwainaina@curalinc.com>
2026-03-12 10:25:10 +0300

    fix:gracefully handle websocket disconnect

--- server.py
+++ server.py
@@ -7,20 +7,19 @@ SERVER = FastAPI()
 async def get_live_events(websocket: WebSocket):
     # later on pass in dynamic user names??? so long as they do not conflict
     await websocket.accept()
-    print('good!')
     try:
         listener_ref: TikTokListener
         while True:
             payload_if_any = await websocket.receive_json()
             username = payload_if_any.get('username')
-            print('username', username)
             action = payload_if_any.get('action')
             if action == 'connect':
                 listener_ref = TikTokListener(websocket, f'{username}')
                 await listener_ref.start_client()
             elif action == 'disconnect':
                 await listener_ref.close_client()
+    except WebSocketDisconnect:
+        await listener_ref.close_client()
     except Exception as e:
-        print('e', e)
         pass
 
